7b6913a84001643b99f4e7e566eace063bb6f501
Alex <alexeytitovwork@gmail.com>
2022-01-21 18:57:48 +0300

    fix

--- loader_torch_init_step.cpp
+++ loader_torch_init_step.cpp
@@ -19,7 +19,7 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui.hpp>
 #include "opencv2/core.hpp"
-#define LOG_FLAG False
+#define LOG_FLAG false
 #define TIMERS_FLAG true
 
 using namespace std;
