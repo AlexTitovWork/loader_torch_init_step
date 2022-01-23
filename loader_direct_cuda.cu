a1ab847ca9f6007e46fb9775e37586759adb7f5b
Alex <alexeytitovwork@gmail.com>
2022-01-23 22:06:57 +0300

    Driver control

--- loader_direct_cuda.cu
+++ loader_direct_cuda.cu
@@ -20,8 +20,13 @@
 #include "opencv2/core.hpp"
 #define LOG_FLAG false
 #define TIMERS_FLAG true
-using namespace std;
 //-----------------------
+//Version CUDA+Driver
+#include <cuda_runtime.h>
+// #include <helper_cuda.h>
+//-----------------------
+using namespace std;
+
 /*
  * cmd line build:
  # For the Docker build
@@ -99,7 +104,43 @@ void kernel(float num, float *a, int N)
 
 int main(int argc, const char *argv[]){
 
+ 
+ 
+
+  int nDevices;
+  int driverVersion = 0;
+  int runtimeVersion = 0;
+  cudaGetDeviceCount(&nDevices);
+
+  for (int i = 0; i < nDevices; i++) {
+    cudaDeviceProp prop;
+    cudaGetDeviceProperties(&prop, i);
+    printf("Device Number: %d\n", i);
+    printf("  Device name: %s\n", prop.name);
+    printf("  Memory Clock Rate (KHz): %d\n",
+           prop.memoryClockRate);
+    printf("  Memory Bus Width (bits): %d\n",
+           prop.memoryBusWidth);
+    printf("  Peak Memory Bandwidth (GB/s): %f\n\n",
+           2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
+    // Console log
+
+    cudaDriverGetVersion(&driverVersion);
+    cudaRuntimeGetVersion(&runtimeVersion);
+    printf("  CUDA Driver Version / Runtime Version          %d.%d / %d.%d\n",
+            driverVersion / 1000, (driverVersion % 100) / 10,
+            runtimeVersion / 1000, (runtimeVersion % 100) / 10);
+
+           printf("  CUDA Capability Major/Minor version number:    %d.%d\n",
+           prop.major, prop.minor);
+    }
+
+    printf("________________________________________________\n\n");
+
     std::cout<< "CUDA direct data transfer() and init() C/C++ CUDA tools test.\n";
+
+
+    //-----------------------------------------------------------------------------------
     int N = 2048*2;
     float *host_a, *device_a;        // Define host-specific and device-specific arrays.
     int size = sizeof(float) * N;
