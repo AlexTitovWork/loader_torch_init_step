d47af3f6de72672ccb648a99e08dbcc5738f4aa4
Alex <alexeytitovwork@gmail.com>
2022-02-10 19:09:09 +0300

    Pin memory

--- loader_torch_init_step.cpp
+++ loader_torch_init_step.cpp
@@ -149,9 +149,9 @@ int main(int argc, const char *argv[]){
     int height =400;
     int width = 400;
     // // torch::cuda::synchronize(-1);
-    // torch::Tensor tensor_image = torch::zeros({1,height,width,3});
+    torch::Tensor tensor_image = torch::zeros({1,height,width,3});
     // // torch::Tensor tensor_image = torch::rand({1,400,400,3});
-    // tensor_image = tensor_image.pin_memory();
+    tensor_image = tensor_image.pin_memory();
 
     //-----------------------------------------------------------------------------
     /**
